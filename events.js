40acbe1aa0d839265c7d8bf8c980e362c1c57086
tomoya19980427goku-pixel <tomoya19980427goku@gmail.com>
2026-07-12 13:51:04 +0900

    events.js

---
+++ events.js
@@ -0,0 +1,97 @@
+const events = [
+  {
+    day: "7/11 (土)",
+    time: "18:00",
+    title: "💃 ワンコインダンス",
+    url: "https://us06web.zoom.us/j/84128790586?pwd=5aqt5DJuxRTDKkn4kmaiRJ9FwcbC9v.1",
+    color: "#9b59b6"
+  },
+
+  {
+    day: "7/12 (日)",
+    time: "13:00",
+    title: "💻 ミナソンクラブ",
+    url: "https://us06web.zoom.us/j/88446459111?pwd=tkdvedhEoE5jn8F1TFc5YCxavxlY94.1",
+    color: "#3498db"
+  },
+
+  {
+    day: "7/13 (月)",
+    time: "12:00",
+    title: "📻 FMなまず",
+    url: "https://us06web.zoom.us/j/81820363801?pwd=o8kVMT44mpZi4S4ENQw6nIPxfUPRgv.1",
+    color: "#f39c12"
+  },
+
+  {
+    day: "7/15 (水)",
+    time: "13:00",
+    title: "🍒 平日チェリーライブ",
+    url: "https://us06web.zoom.us/j/88284911920?pwd=TPXNsyIFr0l814b5QC2mgtDva1AVIx.1",
+    color: "#e74c3c"
+  },
+
+  {
+    day: "7/20 (月)",
+    time: "12:00",
+    title: "📻 FMなまず",
+    url: "",
+    color: "#f39c12"
+  },
+
+  {
+    day: "7/24 (金)",
+    time: "13:00",
+    title: "💻 佐賀オンラインセミナー",
+    url: "",
+    color: "#3498db"
+  },
+
+  {
+    day: "7/26 (日)",
+    time: "13:00",
+    title: "🍒 休日チェリーライブ",
+    url: "",
+    color: "#e74c3c"
+  },
+
+  {
+    day: "7/27 (月)",
+    time: "19:00",
+    title: "📻 ナイトFMなまず",
+    url: "",
+    color: "#f39c12"
+  },
+
+  {
+    day: "7/28 (火)",
+    time: "19:00",
+    title: "💻 竹の子族",
+    url: "",
+    color: "#3498db"
+  },
+
+  {
+    day: "7/29 (水)",
+    time: "13:00",
+    title: "🌸 サクラ咲く会",
+    url: "",
+    color: "#ff69b4"
+  },
+
+  {
+    day: "7/29 (水)",
+    time: "20:00",
+    title: "🧘 ヨガチャンネル",
+    url: "",
+    color: "#27ae60"
+  },
+
+  {
+    day: "7/31 (金)",
+    time: "19:00",
+    title: "🧪 理科の実験チャンネル",
+    url: "",
+    color: "#16a085"
+  }
+];
