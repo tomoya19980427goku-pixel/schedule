4662e131a5232759a16135262d6aa5c77f0c65cc
erina-zoom <tomoya19980427goku@gmail.com>
2026-07-19 14:46:01 +0900

    ERINA Manager Update

--- data/events.js
+++ data/events.js
@@ -1,66 +1,41 @@
-// ==========================================
-// ERINA Zoom Schedule Ver3
-// イベントデータ
-// ==========================================
-
 const events = [
-
-{
-    year: 2026,
-    month: 7,
-    date: 20,
-    time: "12:00",
-
-    title: "📻 FMなまず",
-    shortTitle: "📻FM",
-
-    image: "images/fm314.jpg",
-
-    url: "https://us06web.zoom.us/j/86184643416?pwd=HRDbK9qJDsIMsXNbUQh2TypatFajN5.1",
-
-    color: "#f39c12",
-
-    // 追加すると便利
-    category: "fm",
-    endTime: "13:30"
-},
-
-{
-    year:2026,
-    month:7,
-    date:24,
-    time:"13:00",
-
-    title:"💻 佐賀オンラインセミナー",
-    shortTitle:"💻佐賀",
-
-    image:"images/saga24.jpg",
-
-    url:"",
-
-    color:"#3498db",
-
-    category:"saga",
-    endTime:"15:00"
-},
-
-{
-    year:2026,
-    month:7,
-    date:26,
-    time:"13:00",
-
-    title:"🍒 休日チェリーライブ",
-    shortTitle:"🍒チェリー",
-
-    image:"images/cherry215.jpg",
-
-    url:"https://us06web.zoom.us/j/81508872311?pwd=i01nYlbwEYec6hF2w1loy4yK0fgjCl.1",
-
-    color:"#e74c3c",
-
-    category:"cherry",
-    endTime:"14:30"
-}
-
+  {
+    "year": 2026,
+    "month": 7,
+    "date": 20,
+    "time": "12:30",
+    "endTime": "13:30",
+    "title": "📻 FMなまず",
+    "shortTitle": "📻FM",
+    "image": "images/fm314.jpg",
+    "url": "https://us06web.zoom.us/j/86184643416?pwd=HRDbK9qJDsIMsXNbUQh2TypatFajN5.1",
+    "color": "#f39c12",
+    "category": "fm"
+  },
+  {
+    "year": 2026,
+    "month": 7,
+    "date": 24,
+    "time": "13:00",
+    "title": "💻 佐賀オンラインセミナー",
+    "shortTitle": "💻佐賀",
+    "image": "images/saga24.jpg",
+    "url": "",
+    "color": "#3498db",
+    "category": "saga",
+    "endTime": "15:00"
+  },
+  {
+    "year": 2026,
+    "month": 7,
+    "date": 26,
+    "time": "13:00",
+    "title": "🍒 休日チェリーライブ",
+    "shortTitle": "🍒チェリー",
+    "image": "images/cherry215.jpg",
+    "url": "https://us06web.zoom.us/j/81508872311?pwd=i01nYlbwEYec6hF2w1loy4yK0fgjCl.1",
+    "color": "#e74c3c",
+    "category": "cherry",
+    "endTime": "14:30"
+  }
 ];
